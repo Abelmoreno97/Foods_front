48266ed7252baae2dc9dc90330522baca5219645
Abelmoreno97 <abelyamilmoreno@gmail.com>
2023-02-11 15:36:38 -0300

    /

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from './redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://foods-back-prod.up.railway.app/'
+axios.defaults.baseURL = 'https://foods-back-prod.up.railway.app'
 
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>,document.getElementById('root'));
